d6d2ddac84a4ca97d4834fb3c3cc534eea59de4d
milad.cia <milad.cia@gmail.com>
2025-12-10 09:59:28 +0330

    procedure to view

--- router/settingApi.js
+++ router/settingApi.js
@@ -502,9 +502,9 @@ router.post('/list-faktors', auth, async (req, res) => {
 router.post('/edit-faktor', auth, async (req, res) => {
 	try {
         const userId = req.headers['userid'];
-        const { faktorNo, bankDate,bank } = req.body;
+        const { InvoiceID, bankDate,bank } = req.body;
         
-        const result = await faktor.updateOne({faktorNo:faktorNo},
+        const result = await faktor.updateOne({InvoiceID:InvoiceID},
             {bankDate,bank}
         );
 		
