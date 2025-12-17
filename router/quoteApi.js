ec255b8a0a20aa1763aac33ad8a536efdc54098a
milad.cia <milad.cia@gmail.com>
2025-12-17 10:51:30 +0330

    put bank in user

--- router/quoteApi.js
+++ router/quoteApi.js
@@ -1073,8 +1073,12 @@ router.post('/update-Item', jsonParser, async (req, res) => {
                     oldCartItems[i].count = data.changes.count
                 if (data.changes.discount)
                     oldCartItems[i].discount = data.changes.discount
-                if (data.changes.price)
-                    oldCartItems[i].fixPrice = data.changes.price
+                if (data.changes.price){
+                    oldCartItems[i].fixPrice = data.changes.price;
+                    oldCartItems[i].price.forEach(item => {
+                        item.price = data.changes.price;
+                        });
+                } 
 
 
             }
