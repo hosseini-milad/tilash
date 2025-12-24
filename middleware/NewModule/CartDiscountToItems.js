fe4a8bd13531021faab02cd61188832af0512232
milad.cia <milad.cia@gmail.com>
2025-12-24 10:35:10 +0330

    Reg Sanad

--- middleware/NewModule/CartDiscountToItems.js
+++ middleware/NewModule/CartDiscountToItems.js
@@ -3,7 +3,7 @@ const findPayValuePrice = require("./FindPayValuePrice");
 
 const CartDiscountToItems=async(userId,oldCartItems, cartDiscount,totalPrice,pDiscount)=>{
 	var cDiscount = 0
-	var preDiscount = pDiscount?Number(pDiscount):0
+	var preDiscount = 0//pDiscount?Number(pDiscount):0
 	var disCount = Number(cartDiscount)-preDiscount
 	var disPercent= 0
 	//if(disCount>100){
@@ -12,7 +12,7 @@ const CartDiscountToItems=async(userId,oldCartItems, cartDiscount,totalPrice,pDi
 		for (var i = 0; i < (oldCartItems && oldCartItems.length); i++) {
 			var price = findPayValuePrice(oldCartItems[i].price, oldCartItems.payValue)
 			var count = Number(oldCartItems[i].count)
-			var oldDiscount = Number(oldCartItems[i].discount )
+			var oldDiscount = 0//Number(oldCartItems[i].discount )
 			var roundDiscount = 0
 			if(i == oldCartItems.length-1){
 				roundDiscount = disCount - cDiscount
