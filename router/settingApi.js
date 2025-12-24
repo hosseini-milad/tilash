8da6d4ea73fd9a6c9ddc23d29e864c9aa79a2c17
milad.cia <milad.cia@gmail.com>
2025-12-24 11:20:42 +0330

    Reg Sanad

--- router/settingApi.js
+++ router/settingApi.js
@@ -615,6 +615,8 @@ router.post('/list-faktors', auth, async (req, res) => {
             var bankData = cartData.bank&&
             await bankAccounts.findOne({ BankAccountID: cartData.bank})
             filter[i].bankName = bankData&&bankData.DlTitle
+            const customerDetail = await customer.findOne({_id:ObjectID(cartData.userId)})
+            filter[i].customer = customerDetail
         }
 		const bankList = await bankAccounts.find({ limit: adminData.username }).lean();
 		return res.json({ filter, tabs, size, bankList });
