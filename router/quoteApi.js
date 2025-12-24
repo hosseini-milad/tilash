deac21517d6657a1845c0a2ff52d44b34175115c
milad.cia <milad.cia@gmail.com>
2025-12-24 18:39:57 +0330

    quote fix

--- router/quoteApi.js
+++ router/quoteApi.js
@@ -1791,8 +1791,8 @@ router.post('/update-desc', jsonParser, async (req, res) => {
                     cartDiscount,totalPrice,qCartData&&qCartData.pDiscount,1
                 )
             }
-            await quickCart.updateOne({ userId }, { ...data });
-
+            const result = await quickCart.updateOne({ userId }, { ...data });
+            console.log(result)
             //return res.json({...dataQuick,disPercent})
         }
 
