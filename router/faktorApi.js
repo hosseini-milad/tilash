4815343428728f9136a6f9f5c22886a738caf88b
milad.cia <milad.cia@gmail.com>
2025-12-24 19:41:55 +0330

    Reg Sanad

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -916,6 +916,10 @@ const findCartFunction = async (userIdRaw, manageId, pageSize = 10, offset = 0,
 				cartDetail.push(findCartSum(cartData[c].cartItems, 
                     cartData[c].payValue,cartData[c].transportPrice,
                     cartData[c].discount));
+                if(cartData[c].InvoiceID)
+                    cartData[c].canEdit = 0
+                else
+                    cartData[c].canEdit = 1
 			} catch {}
 		}
 		if (qCartData) {
