bfad160abe06fe9ae4fba1f7175df774be1954e1
hosseini.milad1400 <milad.cia@gmail.com>
2025-12-25 09:10:03 +0330

    faktor value

--- router/settingApi.js
+++ router/settingApi.js
@@ -31,6 +31,7 @@ const bankAccounts = require('../models/product/bankAccounts');
 const utils = require('../utils');
 const customerModel = require('../models/auth/customers');
 const userModel = require('../models/auth/users');
+const CalcFaktorRemain = require('../middleware/NewModule/CalcFaktorRemain');
 //const UpdateExcel = require('../middleware/UpdateExcel');
 
 router.post('/sliders', async (req, res) => {
@@ -547,6 +548,16 @@ router.post('/list-faktors', auth, async (req, res) => {
             var bankData = cartData.bank&&
             await bankAccounts.findOne({ BankAccountID: cartData.bank})
             filter[i].bankName = bankData&&bankData.DlTitle
+            const customerDetail = await customers.findOne({_id:ObjectID(cartData.userId)})
+            filter[i].customer = customerDetail
+            filter[i].remainPrice = CalcFaktorRemain(cartData.NetPrice,cartData.bankArray)
+            
+            if(filter[i].remainPrice&&filter[i].ReceiptID)
+                filter[i].hasRemain = true
+            else
+                filter[i].hasRemain = false
+            if(filter[i].remainPrice == "-")
+                filter[i].hasRemain = true
         }
 		const bankList = await bankAccounts.find({ limit: adminData.username }).lean();
 		return res.json({ filter, tabs, size, bankList });
