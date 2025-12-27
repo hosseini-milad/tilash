aa3fdb2d43a99ca8e9c3bff740a9b5a515a13354
hosseini.milad1400 <milad.cia@gmail.com>
2025-12-27 10:35:07 +0330

    discount check

--- middleware/NewModule/SepidarDiscountToItems.js
+++ middleware/NewModule/SepidarDiscountToItems.js
@@ -11,14 +11,14 @@ const SepidarDiscountToItems=(oldCartItems, cartDiscount,totalPrice)=>{
 		for (var i = 0; i < (oldCartItems && oldCartItems.length); i++) {
 			var price = findPayValuePrice(oldCartItems[i].price, oldCartItems.payValue)
 			var count = Number(oldCartItems[i].count)
-			var oldDiscount = 0//Number(oldCartItems[i].discount )
+			var oldDiscount = Number(oldCartItems[i].discount )
 			var roundDiscount = 0
 			if(i == oldCartItems.length-1){
 				roundDiscount = disCount - cDiscount
 			}
 			else roundDiscount= (parseInt(Number(disPercent*price*count)/10000)*10000)
 			var newDiscount = oldDiscount + roundDiscount
-			cDiscount += newDiscount
+			cDiscount += roundDiscount
 			oldCartItems[i].discount = newDiscount;
 		}
 		return oldCartItems
