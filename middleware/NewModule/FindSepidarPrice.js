b604de3956d0ce192a7ad9cb2369ca2291588214
milad.cia <milad.cia@gmail.com>
2026-01-01 12:19:16 +0330

    Reg Sanad

---
+++ middleware/NewModule/FindSepidarPrice.js
@@ -0,0 +1,31 @@
+const qCart = require("../../models/product/quickCart");
+const products = require("../../models/product/products");
+const findQCartSum = require("./FindQuickCartSum");
+const findCartItemDetail = require("./FindQuickItem");
+const quoteApi = require('../../models/product/quote');
+
+const FindQuick=async(userId,noDiscount,isQuote)=>{
+    if(!userId) return('')
+        try{
+        var qCartDetail= {}
+        const qCartData = isQuote?await quoteApi.findOne({userId:userId}).lean():
+		await qCart.findOne({userId:userId}).lean()
+        if (qCartData) {
+			for (let j = 0; j < qCartData.cartItems.length; j++) {
+				try {	const cartTemp = qCartData.cartItems[j];
+					const productData = await products.findOne({ sku: cartTemp.sku }).lean();
+					const cartItemDetail = findCartItemDetail(cartTemp, qCartData.payValue, noDiscount?0:qCartData.discount);
+					qCartData.cartItems[j].total = cartItemDetail;
+					qCartData.cartItems[j].productData = productData;
+				} catch {}
+			}
+			qCartDetail = await findQCartSum(qCartData.cartItems, qCartData.payValue, 
+                qCartData.discount, qCartData.transportPrice);
+                
+            return({qCartDetail,qCartData})
+		}
+    }
+    catch{ return('')}
+
+}
+module.exports =FindQuick
