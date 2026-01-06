2f72d20a725abdf8a94bacb6920db93ec3b0ba0d
hosseini.milad1400 <milad.cia@gmail.com>
2026-01-06 15:04:55 +0330

    discount

--- router/faktorApi.js
+++ router/faktorApi.js
@@ -1857,12 +1857,11 @@ router.post('/delete-discount', jsonParser,auth, async (req, res) => {
         const userId = req.body.userId ? req.body.userId : req.headers['userid'];
         const cartNo = req.body.cartNo
         const cartData = cartNo?await cart.findOne({cartNo:cartNo}).lean():
-            quickCart.findOne({userId:userId}).lean()
+            await quickCart.findOne({userId:userId}).lean()
         if(!cartData){
             return res.status(400).json({error:"سفارش پیدا نشد"})
 
         }
-        return res.json({data:cartData})
         var newCartItems = cartData.cartItems
         for(var i=0;i<newCartItems.length;i++){
             newCartItems[i].discount = 0
